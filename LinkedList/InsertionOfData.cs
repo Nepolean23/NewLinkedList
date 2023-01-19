2fcf5ce8ef343ce84b2c85fd3ddc059dabab3c6f
Nepolean23 <nlean1996@gmail.com>
2023-01-19 14:20:06 +0530

    [NEPOLEAN]-Code for insertion of data in LinkedList

---
+++ LinkedList/InsertionOfData.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class InsertionOfData
+    {
+        internal Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted in to linked list: ", node.data);
+        }
+
+
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("The linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+        internal Node InsertionOfDataAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid position");
+            }
+
+            if (position > 1)
+            {
+                var newnode = new Node(data);
+                newnode.next = this.head;
+                head = newnode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head;
+                        head = node;
+                        break;
+
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("Position is out of range");
+                }
+
+            }
+            return head;
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,9 +8,16 @@ using LinkedListProgram;
 //list.Add(70);
 
 //Displaying of linkedlist
+//AppendingOfLinkedList linkedList = new AppendingOfLinkedList();
+//linkedList.Add(56);
+//linkedList.Add(30);
+//linkedList.Add(70);
+//linkedList.Display();
 
-AppendingOfLinkedList linkedList = new AppendingOfLinkedList();
-linkedList.Add(56);
-linkedList.Add(30);
-linkedList.Add(70);
-linkedList.Display();
+//InsertionOfDataAtParticularPosition
+InsertionOfData insert=new InsertionOfData();
+insert.Add(56);
+insert.Add(30);
+insert.Add(70);
+insert.InsertionOfDataAtParticularPosition(2, 30);
+insert.Display();
