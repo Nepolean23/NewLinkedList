fa1ac8e42939a9db74cf2eb27ba13b6119193218
Nepolean23 <nlean1996@gmail.com>
2023-01-19 13:10:37 +0530

    [NEPOLEAN]-Code for Creating of a SimpleLinkedList

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using LinkedListProgram;
+
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
