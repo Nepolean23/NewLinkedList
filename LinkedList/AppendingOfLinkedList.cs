ddb35a86de53ecd03d878aedb2885df0c07c9586
Nepolean23 <nlean1996@gmail.com>
2023-01-19 13:42:44 +0530

    [NEPOLEAN]-Code for Appending of LinkedList Data

---
+++ LinkedList/AppendingOfLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    internal class AppendingOfLinkedList
+    {
+        internal Node head;
+
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted in to linked list: ", node.data);
+        }
+
+
+        internal void Display()
+        {
+            Node temp = this.head;
+
+            if(temp==null)
+            {
+                Console.WriteLine("The linked list is empty");
+                return;
+            }
+            while(temp!= null) 
+            {
+                Console.Write(temp.data+ " ");
+                temp= temp.next;
+            }
+
+        }
+    }
+}
+
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,7 +1,16 @@
 ﻿
 using LinkedListProgram;
 
-LinkedList list = new LinkedList();
-list.Add(56);
-list.Add(30);
-list.Add(70);
+//Creating of simple linked list
+//LinkedList list = new LinkedList();
+//list.Add(56);
+//list.Add(30);
+//list.Add(70);
+
+//Displaying of linkedlist
+
+AppendingOfLinkedList linkedList = new AppendingOfLinkedList();
+linkedList.Add(56);
+linkedList.Add(30);
+linkedList.Add(70);
+linkedList.Display();
